92af16f19b6f7eedd225a56d7c38049629717415
ismailsirakov <isosirakov@gmail.com>
2021-07-09 15:30:45 +0300

    Create the project

---
+++ EateryPOSSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace EateryPOSSystem.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
